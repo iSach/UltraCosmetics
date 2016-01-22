9e2af5c0f5f960d23f8fdb5b469a6c44cf2573dd
JHXSMatthew <68638023@qq.com>
2016-01-23 07:36:01 +0800

    Add some fun stuff to flyingboat

--- src/main/java/be/isach/ultracosmetics/cosmetics/mounts/MountFlyingBoat.java
+++ src/main/java/be/isach/ultracosmetics/cosmetics/mounts/MountFlyingBoat.java
@@ -1,15 +1,26 @@
 package be.isach.ultracosmetics.cosmetics.mounts;
 
 import be.isach.ultracosmetics.Core;
+import be.isach.ultracosmetics.util.MathUtils;
 import be.isach.ultracosmetics.util.Particles;
 import be.isach.ultracosmetics.util.UtilParticles;
 import net.minecraft.server.v1_8_R3.EntityBoat;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Sound;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftBoat;
+import org.bukkit.entity.Animals;
+import org.bukkit.entity.Chicken;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Sheep;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.vehicle.VehicleDestroyEvent;
 import org.bukkit.util.Vector;
 
@@ -21,6 +32,9 @@ import java.util.UUID;
  */
 public class MountFlyingBoat extends Mount {
 
+	long nextAllowTime = 0;
+	Entity currentboom = null;
+	
     public MountFlyingBoat(UUID owner) {
         super(owner, MountType.FLYINGBOAT
         );
@@ -35,7 +49,7 @@ public class MountFlyingBoat extends Mount {
      
         EntityBoat ec = ((CraftBoat) ent).getHandle();
 
-
+     
         Vector vector = getPlayer().getLocation().toVector();
 
         double rotX = getPlayer().getLocation().getYaw();
@@ -51,7 +65,32 @@ public class MountFlyingBoat extends Mount {
         ec.getBukkitEntity().setVelocity(vector);
 
         ec.pitch = getPlayer().getLocation().getPitch();
-        ec.yaw = getPlayer().getLocation().getYaw() - 180;        
+        ec.yaw = getPlayer().getLocation().getYaw() - 180;
+        if(currentboom != null){
+        	if(currentboom.isDead()){
+        		currentboom = null;
+        		return;
+        	}
+        	currentboom.getWorld().playSound(currentboom.getLocation(), Sound.CLICK, 1, 1);
+        	if(currentboom.isOnGround()){
+        		Location l = currentboom.getLocation().clone();
+        		for(Entity i : currentboom.getNearbyEntities(3, 3, 3)){
+                    double dX = i.getLocation().getX() - currentboom.getLocation().getX();
+                    double dY = i.getLocation().getY() - currentboom.getLocation().getY();
+                    double dZ = i.getLocation().getZ() - currentboom.getLocation().getZ();
+                    double yaw = Math.atan2(dZ, dX);
+                    double pitch = Math.atan2(Math.sqrt(dZ * dZ + dX * dX), dY) + Math.PI;
+                    double X = Math.sin(pitch) * Math.cos(yaw);
+                    double Y = Math.sin(pitch) * Math.sin(yaw);
+                    double Z = Math.cos(pitch);
+        			MathUtils.applyVelocity(i,  new Vector(X, Z, Y));
+        		}
+        		UtilParticles.display(Particles.EXPLOSION_HUGE, l);
+        		l.getWorld().playSound(l, Sound.EXPLODE, 1, 1);
+        		currentboom.remove();
+        		currentboom = null;
+        	}
+        }
     }
 
     @EventHandler
@@ -62,6 +101,39 @@ public class MountFlyingBoat extends Mount {
 
     }
 
+    @EventHandler
+    public void onInteractEvent(PlayerInteractEvent event){
+    	if(event.getAction() == Action.PHYSICAL ){
+    		return;
+    	}
+    	if(event.getPlayer() != getPlayer()){
+    		return;
+    	}
+    	if(event.getPlayer().getVehicle() == null  || event.getPlayer().getVehicle() != ent){
+    		return;
+    	}
+    	
+    	if(System.currentTimeMillis() < nextAllowTime){
+    		getPlayer().playSound(getPlayer().getLocation(), Sound.ITEM_PICKUP, 1, 1);
+    		return;
+    	}
+    	
+		getPlayer().playSound(getPlayer().getLocation(), Sound.CLICK, 1, 1);
+    	nextAllowTime = System.currentTimeMillis() + 10000;
+    	currentboom = getPlayer().getWorld().spawnEntity(getPlayer().getLocation(), EntityType.PRIMED_TNT);
+    	currentboom.setCustomName(ChatColor.RED+ ChatColor.BOLD.toString() + "!");
+    	currentboom.setCustomNameVisible(true);
+    	
+    	if(currentboom instanceof LivingEntity){
+    		((LivingEntity)currentboom).setNoDamageTicks(-1);
+    		if(currentboom instanceof Animals){
+    			((Animals)currentboom).setBreed(false);
+    		}
+    	}
+    	
+    	
+    }
+    
     @EventHandler
     public void onEntityDamage(EntityDamageByEntityEvent event) {
         Entity e = event.getDamager();
@@ -77,4 +149,11 @@ public class MountFlyingBoat extends Mount {
     		event.setCancelled(true);
     	}
     }
+    @Override
+    public void onClear(){
+    	if(currentboom != null){
+    		currentboom.remove();
+    	}
+    }
+    
 }
