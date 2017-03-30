7630128261c76e8908bda5fad231bc0318d51183
RadBuilder <RadBuilder@users.noreply.github.com>
2017-03-30 14:20:05 -0700

    Fixed #252

--- core/src/main/java/be/isach/ultracosmetics/cosmetics/gadgets/GadgetAntiGravity.java
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/gadgets/GadgetAntiGravity.java
@@ -1,8 +1,8 @@
 package be.isach.ultracosmetics.cosmetics.gadgets;
 
 import be.isach.ultracosmetics.UltraCosmetics;
-import be.isach.ultracosmetics.player.UltraPlayer;
 import be.isach.ultracosmetics.cosmetics.type.GadgetType;
+import be.isach.ultracosmetics.player.UltraPlayer;
 import be.isach.ultracosmetics.util.MathUtils;
 import be.isach.ultracosmetics.util.Particles;
 import be.isach.ultracosmetics.util.UtilParticles;
@@ -12,7 +12,6 @@ import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
@@ -40,28 +39,28 @@ public class GadgetAntiGravity extends Gadget {
         running = true;
         as.setVisible(false);
         as.setHelmet(new ItemStack(Material.SEA_LANTERN));
-        Bukkit.getScheduler().runTaskLater(getUltraCosmetics(), () -> {
-            as.remove();
-            as = null;
-            Bukkit.getScheduler().runTaskLater(getUltraCosmetics(), () -> running = false, 20);
-        }, 220);
+        Bukkit.getScheduler().runTaskLater(getUltraCosmetics(), () -> running = false, 240);
     }
 
     @Override
     void onLeftClick() {
-
     }
 
     @Override
     public void onUpdate() {
         if (as != null && as.isValid()) {
-            as.setHeadPose(as.getHeadPose().add(0, 0.1, 0));
-            UtilParticles.display(Particles.PORTAL, 3f, 3f, 3f, as.getLocation(), 150);
-            UtilParticles.display(Particles.SPELL_WITCH, .3f, .3f, .3f, as.getEyeLocation(), 5);
-            for (Entity ent : as.getNearbyEntities(3, 2, 3)) {
-                if (ent instanceof LivingEntity && !(ent instanceof ArmorStand)) {
-                    MathUtils.applyVelocity(ent, new Vector(0, 0.05, 0));
+            if (running) {
+                as.setHeadPose(as.getHeadPose().add(0, 0.1, 0));
+                UtilParticles.display(Particles.PORTAL, 3f, 3f, 3f, as.getLocation(), 150);
+                UtilParticles.display(Particles.SPELL_WITCH, .3f, .3f, .3f, as.getEyeLocation(), 5);
+                for (Entity ent : as.getNearbyEntities(3, 2, 3)) {
+                    if (ent instanceof LivingEntity && !(ent instanceof ArmorStand)) {
+                        MathUtils.applyVelocity(ent, new Vector(0, 0.05, 0));
+                    }
                 }
+            } else {
+                as.remove();
+                as = null;
             }
         }
     }
@@ -88,8 +87,6 @@ public class GadgetAntiGravity extends Gadget {
 
     @Override
     public void onClear() {
-        if (as != null) {
-            as.remove();
-        }
+        running = false;
     }
 }
