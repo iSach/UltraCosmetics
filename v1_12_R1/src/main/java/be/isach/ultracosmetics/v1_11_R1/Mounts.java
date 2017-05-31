5b54e89861b80ce893d7e9ccfac17226054de5db
iSach <sacha.lewin@me.com>
2017-05-31 23:01:03 +0200

    Pre-1.12 support

---
+++ v1_12_R1/src/main/java/be/isach/ultracosmetics/v1_11_R1/Mounts.java
@@ -0,0 +1,56 @@
+package be.isach.ultracosmetics.v1_11_R1;
+
+import be.isach.ultracosmetics.cosmetics.mounts.Mount;
+import be.isach.ultracosmetics.v1_11_R1.mount.*;
+import be.isach.ultracosmetics.version.IMounts;
+import org.bukkit.entity.EntityType;
+
+/**
+ * @author RadBuilder
+ */
+public class Mounts implements IMounts{
+    @Override
+    public Class<? extends Mount> getSquidClass() {
+        return MountSkySquid.class;
+    }
+
+    @Override
+    public Class<? extends Mount> getSpiderClass() {
+        return MountSpider.class;
+    }
+
+    @Override
+    public Class<? extends Mount> getSlimeClass() {
+        return MountSlime.class;
+    }
+
+    @Override
+    public Class<? extends Mount> getHorrorClass() {
+        return MountInfernalHorror.class;
+    }
+
+    @Override
+    public Class<? extends Mount> getWalkingDeadClass() {
+        return MountWalkingDead.class;
+    }
+
+    @Override
+    public Class<? extends Mount> getRudolphClass() {
+        return MountRudolph.class;
+    }
+
+    @Override
+    public EntityType getHorrorType() {
+        return EntityType.SKELETON_HORSE;
+    }
+
+    @Override
+    public EntityType getWalkingDeadType() {
+        return EntityType.ZOMBIE_HORSE;
+    }
+
+    @Override
+    public EntityType getRudolphType() {
+        return EntityType.MULE;
+    }
+}
