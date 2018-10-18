dc52de666d0b74de729c722afb3bd94562d456e9
RadBuilder <RadBuilder@users.noreply.github.com>
2018-10-17 19:03:03 -0700

    Copied 1.12 R1 to 1.13 R1

---
+++ v1_13_R1/src/main/java/be/isach/ultracosmetics/v1_13_R1/AnvilGUI.java
@@ -0,0 +1,36 @@
+package be.isach.ultracosmetics.v1_13_R1;
+
+import be.isach.ultracosmetics.version.AAnvilGUI;
+import net.minecraft.server.v1_13_R1.*;
+import org.bukkit.craftbukkit.v1_13_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+/**
+ * @author RadBuilder
+ */
+public class AnvilGUI extends AAnvilGUI {
+	public AnvilGUI(Player player, AnvilClickEventHandler handler) {
+		super(player, handler);
+	}
+	
+	private class AnvilContainer extends ContainerAnvil {
+		public AnvilContainer(EntityHuman entity) {
+			super(entity.inventory, entity.world, new BlockPosition(0, 0, 0), entity);
+			this.checkReachable = false;
+		}
+	}
+	
+	@Override
+	public void open() {
+		EntityPlayer p = ((CraftPlayer) player).getHandle();
+		AnvilContainer container = new AnvilContainer(p);
+		inv = container.getBukkitView().getTopInventory();
+		for (AnvilSlot slot : items.keySet())
+			inv.setItem(slot.getSlot(), items.get(slot));
+		int c = p.nextContainerCounter();
+		p.playerConnection.sendPacket(new PacketPlayOutOpenWindow(c, "minecraft:anvil", new ChatMessage("Repairing"), 0));
+		p.activeContainer = container;
+		p.activeContainer.windowId = c;
+		p.activeContainer.addSlotListener(p);
+	}
+}
---
+++ v1_13_R1/src/main/java/be/isach/ultracosmetics/v1_13_R1/customentities/Pumpling.java
@@ -0,0 +1,85 @@
+package be.isach.ultracosmetics.v1_13_R1.customentities;
+
+import be.isach.ultracosmetics.UltraCosmeticsData;
+import be.isach.ultracosmetics.cosmetics.pets.IPetCustomEntity;
+import be.isach.ultracosmetics.util.Particles;
+import be.isach.ultracosmetics.util.UtilParticles;
+import be.isach.ultracosmetics.v1_13_R1.pets.CustomEntityPet;
+import net.minecraft.server.v1_13_R1.*;
+import org.bukkit.entity.Zombie;
+
+/**
+ * @author RadBuilder
+ */
+public class Pumpling extends EntityZombie implements IPetCustomEntity {
+	
+	private CustomEntityPet pet = null;
+	
+	public Pumpling(World world) {
+		super(world);
+	}
+	
+	public Pumpling(World world, CustomEntityPet pet) {
+		super(world);
+		this.pet = pet;
+	}
+	
+	public org.bukkit.entity.Entity getEntity() {
+		return getBukkitEntity();
+	}
+	
+	@Override
+	protected SoundEffect F() { // say
+		if (isCustomEntity()) {
+			a(SoundEffects.bM, 0.05f, 2f);
+			return null;
+		} else return super.F();
+	}
+	
+	@Override
+	protected SoundEffect d(DamageSource damageSource) { // Hurt
+		if (isCustomEntity()) return null;
+		else return super.d(damageSource);
+	}
+	
+	@Override
+	protected SoundEffect cf() { // Death
+		if (isCustomEntity()) return null;
+		else return super.cf();
+	}
+	
+	@Override
+	protected SoundEffect dm() { // Step
+		if (isCustomEntity()) return null;
+		else return super.dm();
+	}
+	
+	@Override
+	protected void a(BlockPosition blockposition, Block block) {
+		if (isCustomEntity()) return;
+		super.a(blockposition, block);
+	}
+	
+	@Override
+	public String getName() {
+		return LocaleI18n.get("entity.Zombie.name");
+	}
+	
+	@Override
+	public void B_() {
+		super.B_();
+		if (!isCustomEntity()) return;
+		fireTicks = 0;
+		UtilParticles.display(Particles.FLAME, 0.2f, 0.2f, 0.2f, ((Zombie) getBukkitEntity()).getEyeLocation(), 3);
+		UltraCosmeticsData.get().getVersionManager().getPathfinderUtil().removePathFinders(getBukkitEntity());
+		pet.getFollowTask().follow(pet.getPlayer());
+		setInvisible(true);
+		setBaby(true);
+		setSlot(EnumItemSlot.HEAD, new ItemStack(Blocks.PUMPKIN));
+	}
+	
+	private boolean isCustomEntity() {
+		return CustomEntities.customEntities.contains(this);
+	}
+	
+}
