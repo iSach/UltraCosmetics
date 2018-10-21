c20f4a2ca59e7d072bd9fe5a81bcc6fd6cc5ee60
montlikadani <montlikada@gmail.com>
2018-10-21 14:03:13 +0200

    Update SubCommandReload.java

--- core/src/main/java/be/isach/ultracosmetics/command/subcommands/SubCommandReload.java
+++ core/src/main/java/be/isach/ultracosmetics/command/subcommands/SubCommandReload.java
@@ -6,8 +6,11 @@ import be.isach.ultracosmetics.command.SubCommand;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
 
 public class SubCommandReload extends SubCommand {
+
+	private UltraCosmetics uc;
 
 	public SubCommandReload(UltraCosmetics ultraCosmetics) {
 		super("Reload the plugin and config.", "ultracosmetics.command.reload", "/uc reload", ultraCosmetics, "reload");
@@ -24,7 +27,18 @@ public class SubCommandReload extends SubCommand {
 	}
 
 	private void common(CommandSender sender, String... args) {
-		SettingsManager.getConfig().loadConfiguration(getUltraCosmetics().getFile());
-		sender.sendMessage(org.bukkit.ChatColor.GREEN + "Plugin has been loaded successfully.");
+		SettingsManager.getConfig().loadConfiguration(uc.getFile());
+
+		for (be.isach.ultracosmetics.player.UltraPlayer pl : uc.getPlayerManager().getUltraPlayers()) {
+			if (pl.getInventory().getTitle().equalsIgnoreCase(uc.getMenus().getMainMenu().getName())) {
+				pl.closeInventory();
+				//sender.sendMessage(ChatColor.RED + "The cosmetics menu has been closed, because reloading the plugin.");
+			}
+			uc.getPlayerManager().getUltraPlayer(pl).removeMenuItem();
+
+			uc.getPlayerManager().getUltraPlayer(pl).giveMenuItem();
+			uc.getPlayerManager().getUltraPlayer(pl).updateInventory();
+		}
+		sender.sendMessage(ChatColor.GREEN + "Plugin has been loaded successfully.");
 	}
 }
