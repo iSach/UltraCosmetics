edbee99255df7daaa80a3d21bdbf03faaaabe9b9
iSach <sacha.lewin@me.com>
2019-08-22 17:54:06 +0200

    Fixes...

--- core/src/main/java/be/isach/ultracosmetics/cosmetics/mounts/MountHypeCart.java
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/mounts/MountHypeCart.java
@@ -8,6 +8,7 @@ import be.isach.ultracosmetics.util.PlayerUtils;
 import org.bukkit.entity.Minecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.vehicle.VehicleDestroyEvent;
+import org.bukkit.util.Vector;
 
 /**
  * Represents an instance of a hype cart mount.
@@ -23,8 +24,16 @@ public class MountHypeCart extends Mount<Minecart> {
 
     @Override
     public void onUpdate() {
-        if (entity.isOnGround())
-            entity.setVelocity(PlayerUtils.getHorizontalDirection(getPlayer(), 7.6));
+        if (entity.isOnGround()) {
+            Vector vector = PlayerUtils.getHorizontalDirection(getPlayer(), 7.6);
+            if(Math.abs(vector.getX()) > 4) {
+                vector.setX(vector.getX() < 0 ? -4 : 4);
+            }
+            if(Math.abs(vector.getZ()) > 4) {
+                vector.setZ(vector.getZ() < 0 ? -4 : 4);
+            }
+            entity.setVelocity(vector);
+        }
         UltraCosmeticsData.get().getVersionManager().getEntityUtil().setClimb(entity);
     }
 
