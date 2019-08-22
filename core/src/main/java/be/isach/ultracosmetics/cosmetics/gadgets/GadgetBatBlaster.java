51d9d344e4bbb8fd8283393270a434c0caabf712
iSach <sacha.lewin@me.com>
2019-08-23 01:46:31 +0200

    meh fix

--- core/src/main/java/be/isach/ultracosmetics/cosmetics/gadgets/GadgetBatBlaster.java
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/gadgets/GadgetBatBlaster.java
@@ -62,45 +62,49 @@ public class GadgetBatBlaster extends Gadget {
 
     @Override
     public void onUpdate() {
-        if (active && bats != null && !bats.isEmpty()) {
-            bats.stream().filter(Entity::isValid).forEach(bat -> {
-                Vector rand = new Vector((Math.random() - 0.5D) / 3.0D, (Math.random() - 0.5D) / 3.0D,
-                        (Math.random() - 0.5D) / 3.0D);
-                if (bat != null && playerVelocity != null) {
-                    bat.setVelocity(playerVelocity.getDirection().clone().multiply(0.5D).add(rand));
-                }
-                getPlayer().getWorld().getPlayers().stream()
-                        .filter(other -> !other.equals(getPlayer()) && getOwner().hasGadgetsEnabled() && hitPlayer(
-                                bat.getLocation(), other)).forEachOrdered(other -> {
-
-                    Vector v = bat.getLocation().getDirection();
-                    v.normalize();
-                    v.multiply(.4d);
-                    v.setY(v.getY() + 0.2d);
-
-                    if (v.getY() > 7.5) {
-                        v.setY(7.5);
+        try {
+            if (active && bats != null && !bats.isEmpty()) {
+                bats.stream().filter(Entity::isValid).forEach(bat -> {
+                    Vector rand = new Vector((Math.random() - 0.5D) / 3.0D, (Math.random() - 0.5D) / 3.0D,
+                            (Math.random() - 0.5D) / 3.0D);
+                    if (bat != null && playerVelocity != null) {
+                        bat.setVelocity(playerVelocity.getDirection().clone().multiply(0.5D).add(rand));
                     }
+                    getPlayer().getWorld().getPlayers().stream()
+                            .filter(other -> !other.equals(getPlayer()) && getOwner().hasGadgetsEnabled() && hitPlayer(
+                                    bat.getLocation(), other)).forEachOrdered(other -> {
 
-                    if (other.isOnGround()) {
-                        v.setY(v.getY() + 0.4d);
-                    }
+                        Vector v = bat.getLocation().getDirection();
+                        v.normalize();
+                        v.multiply(.4d);
+                        v.setY(v.getY() + 0.2d);
 
-                    other.setFallDistance(0);
+                        if (v.getY() > 7.5) {
+                            v.setY(7.5);
+                        }
 
-                    if (affectPlayers) {
-                        MathUtils.applyVelocity(other, bat.getLocation().getDirection().add(new Vector(0, .4f, 0)));
-                    }
+                        if (other.isOnGround()) {
+                            v.setY(v.getY() + 0.4d);
+                        }
 
-                    SoundUtil.playSound(bat.getLocation(), Sounds.BAT_HURT, 1.0f, 1.0f);
-                    UtilParticles.display(Particles.SMOKE_NORMAL, bat.getLocation());
+                        other.setFallDistance(0);
 
-                    bat.remove();
+                        if (affectPlayers) {
+                            MathUtils.applyVelocity(other, bat.getLocation().getDirection().add(new Vector(0, .4f, 0)));
+                        }
+
+                        SoundUtil.playSound(bat.getLocation(), Sounds.BAT_HURT, 1.0f, 1.0f);
+                        UtilParticles.display(Particles.SMOKE_NORMAL, bat.getLocation());
+
+                        bat.remove();
+                    });
                 });
-            });
-        } else {
-            playerVelocity = null;
-            clean();
+            } else {
+                playerVelocity = null;
+                clean();
+            }
+        } catch (Exception e) {
+            // TODO
         }
     }
 
