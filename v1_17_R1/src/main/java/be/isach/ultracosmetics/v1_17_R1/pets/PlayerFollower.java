4c4ed51551c19a712bb4870ec2e947a63381971f
datatags <datatags@users.noreply.github.com>
2021-07-29 00:19:45 -0700

    Add Paper compat; fix pets not following you anymore

--- v1_17_R1/src/main/java/be/isach/ultracosmetics/v1_17_R1/pets/PlayerFollower.java
+++ v1_17_R1/src/main/java/be/isach/ultracosmetics/v1_17_R1/pets/PlayerFollower.java
@@ -5,7 +5,11 @@ import be.isach.ultracosmetics.cosmetics.pets.IPlayerFollower;
 import be.isach.ultracosmetics.cosmetics.pets.Pet;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.navigation.PathNavigation;
 import net.minecraft.world.level.pathfinder.Path;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -20,6 +24,7 @@ public class PlayerFollower implements Runnable, IPlayerFollower {
 
     private final Pet pet;
     private final Player player;
+    private static Method pathMethod = null;
 
     public PlayerFollower(Pet pet, Player player) {
         this.pet = pet;
@@ -58,7 +63,7 @@ public class PlayerFollower implements Runnable, IPlayerFollower {
 
             ((Mob) petEntity).getNavigation().setSpeedModifier(2d);
             Location targetLocation = player.getLocation();
-            Path path = ((Mob) petEntity).getNavigation().createPath(targetLocation.getX() + 1, targetLocation.getY(), targetLocation.getZ() + 1, 1);
+            Path path = path((Mob)petEntity, targetLocation);
 
             try {
                 int distance = (int) Bukkit.getPlayer(player.getName()).getLocation().distance(petEntity.getBukkitEntity().getLocation());
@@ -92,5 +97,34 @@ public class PlayerFollower implements Runnable, IPlayerFollower {
     @Override
     public Runnable getTask() {
         return this;
+    }
+    
+    private Path path(Mob mob, Location loc) {
+    	double x = loc.getX() + 1;
+    	double y = loc.getY();
+    	double z = loc.getZ() + 1;
+    	if (pathMethod == null) {
+    		try {
+    			// yes, this seems pretty weird when you're looking at the non-obfuscated version of this class
+    			// this whole method is for compatibility with Paper, because they don't reobfuscate some methods correctly
+    			// you should remove the reflection when this issue is closed:
+    			// https://github.com/PaperMC/paperweight/issues/24
+    			return mob.getNavigation().createPath(x, y, z, 1);
+    		} catch (NoSuchMethodError e) {
+    			try {
+					pathMethod = PathNavigation.class.getDeclaredMethod("createPath", double.class, double.class, double.class, int.class);
+				} catch (NoSuchMethodException | SecurityException e1) {
+					// idk man
+					e1.printStackTrace();
+					return null;
+				}
+    		}
+    	}
+    	try {
+			return (Path) pathMethod.invoke(mob.getNavigation(), x, y, z, 1);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			e.printStackTrace();
+			return null;
+		}
     }
 }
