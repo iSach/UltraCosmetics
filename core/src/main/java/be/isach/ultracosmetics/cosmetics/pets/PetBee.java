0fcc9efadd1c61701c17f7d5deecd75792dbb370
Chris <65610536+Chris6ix@users.noreply.github.com>
2022-01-18 22:24:43 +0100

    Create PetBee.java

---
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/pets/PetBee.java
@@ -0,0 +1,20 @@
+package be.isach.ultracosmetics.cosmetics.pets;
+
+import be.isach.ultracosmetics.UltraCosmetics;
+import be.isach.ultracosmetics.UltraCosmeticsData;
+import be.isach.ultracosmetics.cosmetics.type.PetType;
+import be.isach.ultracosmetics.player.UltraPlayer;
+import be.isach.ultracosmetics.util.ItemFactory;
+import be.isach.ultracosmetics.util.UCMaterial;
+
+/**
+ * Represents an instance of a bee pet summoned by a player.
+ *
+ * @author Chris6ix
+ * @since 18-01-2021
+ */
+public class PetBee extends Pet {
+    public PetBee(UltraPlayer owner, UltraCosmetics ultraCosmetics) {
+        super(owner, ultraCosmetics, PetType.getByName("bee"), ItemFactory.create(UCMaterial.HONEYCOMB, UltraCosmeticsData.get().getItemNoPickupString()));
+    }
+}
