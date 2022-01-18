75c876fe7807f2d1f0e89e5a5b3167584ff98c4c
Chris <65610536+Chris6ix@users.noreply.github.com>
2022-01-18 22:40:14 +0100

    Add Bee Pet to MessageManager.java

--- core/src/main/java/be/isach/ultracosmetics/config/MessageManager.java
+++ core/src/main/java/be/isach/ultracosmetics/config/MessageManager.java
@@ -258,6 +258,8 @@ public class MessageManager {
         addMessage("Pets.Piglin.entity-displayname", "&l%playername%'s Gold Dealer");
         addMessage("Pets.Slime.menu-name", "&a&lSlime");
         addMessage("Pets.Slime.entity-displayname", "&l%playername%'s Slime");
+        addMessage("Pets.Bee.menu-name", "&e&lBee");
+        addMessage("Pets.Bee.entity-displayname", "&l%playername%'s Bee");
         addMessage("Pets.Spawn", "%prefix% &9You spawned %petname%");
         addMessage("Pets.Despawn", "%prefix% &9You despawned %petname%");
 
