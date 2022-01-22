a125f3ca210f8f451eb8f4807ed1cc1ae9a20a6f
Chris <65610536+Chris6ix@users.noreply.github.com>
2022-01-22 18:39:22 +0100

    Temporarily disabled "broken" pets
    Temporarily disabled the Vex, the Axolotl, the Piglin and the Slime Pets until a fix is found

--- core/src/main/java/be/isach/ultracosmetics/cosmetics/type/PetType.java
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/type/PetType.java
@@ -83,14 +83,14 @@ public final class PetType extends CosmeticMatType<Pet> {
         new PetType("ultracosmetics.pets.mooshroom", "Mooshroom", UCMaterial.RED_MUSHROOM, "&7&oMoooo!", EntityType.MUSHROOM_COW, PetMooshroom.class, ServerVersion.earliest());
         
         if (serverVersion.isAtLeast(ServerVersion.v1_17_R1)) {
-            new PetType("ultracosmetics.pets.axolotl", "Axolotl", UCMaterial.AXOLOTL_BUCKET, "&7&oSooo Cute!", EntityType.AXOLOTL, PetAxolotl.class, ServerVersion.v1_17_R1);
+            /*new PetType("ultracosmetics.pets.axolotl", "Axolotl", UCMaterial.AXOLOTL_BUCKET, "&7&oSooo Cute!", EntityType.AXOLOTL, PetAxolotl.class, ServerVersion.v1_17_R1);*/
             /*Temporarily using WHEAT as material for the Goat until https://minecraft.fandom.com/wiki/Goat_Horn comes to Java edition*/
             new PetType("ultracosmetics.pets.goat", "Goat", UCMaterial.WHEAT, "&7&oBAAAA!", EntityType.GOAT, PetGoat.class, ServerVersion.v1_17_R1);
         }
         
-        if (serverVersion.isAtLeast(ServerVersion.v1_16_R3)) {
+        /*if (serverVersion.isAtLeast(ServerVersion.v1_16_R3)) {
             new PetType("ultracosmetics.pets.piglin", "Piglin", UCMaterial.GOLD_INGOT, "&7&oDeals with Gold!", EntityType.PIGLIN, PetPiglin.class, ServerVersion.v1_16_R3);
-        }
+        }*/
         
         if (serverVersion.isAtLeast(ServerVersion.v1_15_R1)) {
             new PetType("ultracosmetics.pets.bee", "Bee", UCMaterial.HONEYCOMB, "&7&o*bzzzz* *bzzzz*", EntityType.BEE, PetBee.class, ServerVersion.v1_15_R1);
@@ -119,12 +119,12 @@ public final class PetType extends CosmeticMatType<Pet> {
             new PetType("ultracosmetics.pets.polarbear", "PolarBear", UCMaterial.SNOW_BLOCK, "&7&oI prefer cold areas", GetForVersion.entityType("POLAR_BEAR"), PetPolarBear.class, ServerVersion.v1_12_R1);
             new PetType("ultracosmetics.pets.llama", "Llama", UCMaterial.RED_WOOL, "&7&oNeed me to carry anything?", GetForVersion.entityType("LLAMA"), PetLlama.class, ServerVersion.v1_12_R1);
             new PetType("ultracosmetics.pets.parrot", "Parrot", UCMaterial.COOKIE, "&7&oPolly want a cracker?", GetForVersion.entityType("PARROT"), PetParrot.class, ServerVersion.v1_12_R1);
-            new PetType("ultracosmetics.pets.vex", "Vex", UCMaterial.IRON_SWORD, "&7&oYAAHH Ehehhehe!", GetForVersion.entityType("VEX"), PetVex.class, ServerVersion.v1_12_R1);
+            /*new PetType("ultracosmetics.pets.vex", "Vex", UCMaterial.IRON_SWORD, "&7&oYAAHH Ehehhehe!", GetForVersion.entityType("VEX"), PetVex.class, ServerVersion.v1_12_R1);*/
         }
 
         new PetType("ultracosmetics.pets.sheep", "Sheep", UCMaterial.WHITE_WOOL, "&7&oBaaaa, baa", EntityType.SHEEP, PetSheep.class, ServerVersion.earliest());
         new PetType("ultracosmetics.pets.wither", "Wither", UCMaterial.WITHER_SKELETON_SKULL, "&7&oWatch out for me..", EntityType.WITHER, PetWither.class, ServerVersion.earliest());
-        new PetType("ultracosmetics.pets.slime", "Slime", UCMaterial.SLIME_BALL, "&7&oSquish...", EntityType.SLIME, PetSlime.class, ServerVersion.earliest());
+        /*new PetType("ultracosmetics.pets.slime", "Slime", UCMaterial.SLIME_BALL, "&7&oSquish...", EntityType.SLIME, PetSlime.class, ServerVersion.earliest());*/
         new PetType("ultracosmetics.pets.silverfish", "Silverfish", UCMaterial.GRAY_DYE, "&7&oLurking in the walls...", EntityType.SILVERFISH, PetSilverfish.class, ServerVersion.earliest());
     }
 }
