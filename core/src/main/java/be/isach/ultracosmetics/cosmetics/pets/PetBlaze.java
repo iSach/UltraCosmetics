c6a6ec28fcf14887912410d22949484312d00867
Chris <65610536+Chris6ix@users.noreply.github.com>
2022-04-06 16:33:48 +0200

    Create PetBlaze.java

---
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/pets/PetBlaze.java
@@ -0,0 +1,20 @@
+package be.isach.ultracosmetics.cosmetics.pets;
+
+import be.isach.ultracosmetics.UltraCosmetics;
+import be.isach.ultracosmetics.UltraCosmeticsData;
+import be.isach.ultracosmetics.cosmetics.type.PetType;
+import be.isach.ultracosmetics.player.UltraPlayer;
+import be.isach.ultracosmetics.util.ItemFactory;
+import be.isach.ultracosmetics.util.XMaterial;
+
+/**
+ * Represents an instance of a blaze pet summoned by a player.
+ *
+ * @author Chris6ix
+ * @since 06-04-2022
+ */
+public class PetBlaze extends Pet {
+    public PetBlaze(UltraPlayer owner, UltraCosmetics ultraCosmetics) {
+        super(owner, ultraCosmetics, PetType.getByName("blaze"), ItemFactory.create(XMaterial.BLAZE_ROD, UltraCosmeticsData.get().getItemNoPickupString()));
+    }
+}
