7b99088064cb57149add3ac624652cd3036e9fb9
Chris <65610536+Chris6ix@users.noreply.github.com>
2022-04-12 19:42:57 +0200

    Create PetSkeleton.java

---
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/pets/PetSkeleton.java
@@ -0,0 +1,20 @@
+package be.isach.ultracosmetics.cosmetics.pets;
+
+import be.isach.ultracosmetics.UltraCosmetics;
+import be.isach.ultracosmetics.UltraCosmeticsData;
+import be.isach.ultracosmetics.cosmetics.type.PetType;
+import be.isach.ultracosmetics.player.UltraPlayer;
+import be.isach.ultracosmetics.util.ItemFactory;
+import be.isach.ultracosmetics.util.XMaterial;
+
+/**
+ * Represents an instance of a skeleton pet summoned by a player.
+ *
+ * @author Chris6ix
+ * @since 12-04-2022
+ */
+public class PetSkeleton extends Pet {
+    public PetSkeleton(UltraPlayer owner, UltraCosmetics ultraCosmetics) {
+        super(owner, ultraCosmetics, PetType.getByName("skeleton"), ItemFactory.create(XMaterial.BOW, UltraCosmeticsData.get().getItemNoPickupString()));
+    }
+}
