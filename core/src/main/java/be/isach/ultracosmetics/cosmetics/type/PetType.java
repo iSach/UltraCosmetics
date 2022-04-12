ec6099a8f4c5df833e1fe2fd5e21958d7f0e1a93
Chris <65610536+Chris6ix@users.noreply.github.com>
2022-04-12 20:25:06 +0200

    Update PetType.java
    Adding it as ServerVersion.earliest as the Enderman comes from before MC 1.0

--- core/src/main/java/be/isach/ultracosmetics/cosmetics/type/PetType.java
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/type/PetType.java
@@ -77,6 +77,7 @@ public final class PetType extends CosmeticEntType<Pet> {
         new PetType("ultracosmetics.pets.blaze", "Blaze", XMaterial.BLAZE_ROD, "&7&oFlying and hot!", EntityType.BLAZE, PetBlaze.class, ServerVersion.earliest());
         new PetType("ultracosmetics.pets.creeper", "Creeper", XMaterial.GUNPOWDER, "&7&oLikes blowing up your favorite Stuff...", EntityType.CREEPER, PetCreeper.class, ServerVersion.earliest());
         new PetType("ultracosmetics.pets.enderman", "Enderman", XMaterial.ENDER_PEARL, "&7&oDont look at it or it will hunt you!", EntityType.ENDERMAN, PetEnderman.class, ServerVersion.earliest());
+        new PetType("ultracosmetics.pets.skeleton", "Skeleton", XMaterial.BOW, "&7&oWatch out, it will try to shoot you!", EntityType.SKELETON, PetSkeleton.class, ServerVersion.earliest());
         
         if (serverVersion.isAtLeast(ServerVersion.v1_17_R1)) {
             /*new PetType("ultracosmetics.pets.axolotl", "Axolotl", XMaterial.AXOLOTL_BUCKET, "&7&oSooo Cute!", EntityType.AXOLOTL, PetAxolotl.class, ServerVersion.v1_17_R1);*/
