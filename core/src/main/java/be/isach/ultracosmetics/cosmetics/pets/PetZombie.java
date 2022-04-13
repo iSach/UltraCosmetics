1e8a5c6f2596dbb40e11a3216cc96936b26d5f74
Chris <65610536+Chris6ix@users.noreply.github.com>
2022-04-13 21:33:23 +0200

    Create PetZombie.java

---
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/pets/PetZombie.java
@@ -0,0 +1,26 @@
+package be.isach.ultracosmetics.cosmetics.pets;
+
+import be.isach.ultracosmetics.UltraCosmetics;
+import be.isach.ultracosmetics.UltraCosmeticsData;
+import be.isach.ultracosmetics.cosmetics.type.PetType;
+import be.isach.ultracosmetics.player.UltraPlayer;
+import be.isach.ultracosmetics.util.ItemFactory;
+import be.isach.ultracosmetics.util.XMaterial;
+
+/**
+ * Represents an instance of a zombie pet summoned by a player.
+ *
+ * @author Chris6ix
+ * @since 13-04-2022
+ */
+public class PetZombie extends Pet {
+    public PetZombie(UltraPlayer owner, UltraCosmetics ultraCosmetics) {
+        super(owner, ultraCosmetics, PetType.getByName("zombie"), ItemFactory.create(XMaterial.ROTTEN_FLESH, UltraCosmeticsData.get().getItemNoPickupString()));
+    }
+    
+    @Override
+    public void onUpdate() {
+        super.onUpdate();
+        entity.setFireTicks(0);
+    }
+}
