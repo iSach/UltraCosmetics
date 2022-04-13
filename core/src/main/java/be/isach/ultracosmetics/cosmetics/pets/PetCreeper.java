47f9edbf79df96ce84c28a69b8829b86207ba896
datatags <datatags@users.noreply.github.com>
2022-04-13 13:09:41 -0700

    Fix imports

--- core/src/main/java/be/isach/ultracosmetics/cosmetics/pets/PetCreeper.java
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/pets/PetCreeper.java
@@ -1,11 +1,12 @@
 package be.isach.ultracosmetics.cosmetics.pets;
 
+import com.cryptomorin.xseries.XMaterial;
+
 import be.isach.ultracosmetics.UltraCosmetics;
 import be.isach.ultracosmetics.UltraCosmeticsData;
 import be.isach.ultracosmetics.cosmetics.type.PetType;
 import be.isach.ultracosmetics.player.UltraPlayer;
 import be.isach.ultracosmetics.util.ItemFactory;
-import be.isach.ultracosmetics.util.XMaterial;
 
 /**
  * Represents an instance of a creeper pet summoned by a player.
