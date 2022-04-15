ee0e23102a399180e1b739ea5831cbad9ea82e4f
datatags <datatags@users.noreply.github.com>
2022-04-14 22:51:26 -0700

    Fix compile problem

--- core/src/main/java/be/isach/ultracosmetics/cosmetics/pets/PetPiglin.java
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/pets/PetPiglin.java
@@ -5,6 +5,9 @@ import be.isach.ultracosmetics.UltraCosmeticsData;
 import be.isach.ultracosmetics.cosmetics.type.PetType;
 import be.isach.ultracosmetics.player.UltraPlayer;
 import be.isach.ultracosmetics.util.ItemFactory;
+
+import org.bukkit.entity.Piglin;
+
 import com.cryptomorin.xseries.XMaterial;
 
 /**
@@ -17,10 +20,10 @@ public class PetPiglin extends Pet {
     public PetPiglin(UltraPlayer owner, UltraCosmetics ultraCosmetics) {
         super(owner, ultraCosmetics, PetType.getByName("piglin"), ItemFactory.create(XMaterial.GOLD_INGOT, UltraCosmeticsData.get().getItemNoPickupString()));
     }
-    
+
     @Override
-    public void onUpdate() {
-        super.onUpdate();
+    public void onEquip() {
+        super.onEquip();
         ((Piglin)entity).setImmuneToZombification(true);
     }
 }
