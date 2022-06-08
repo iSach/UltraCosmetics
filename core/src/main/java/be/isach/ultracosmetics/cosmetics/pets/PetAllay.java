51ebc9417ba39ff8fdc716636a5bbdb9a934424f
Chris <65610536+Chris6ix@users.noreply.github.com>
2022-06-08 02:05:16 +0200

    Create PetAllay.java

---
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/pets/PetAllay.java
@@ -0,0 +1,18 @@
+package be.isach.ultracosmetics.cosmetics.pets;
+
+import be.isach.ultracosmetics.UltraCosmetics;
+import be.isach.ultracosmetics.cosmetics.type.PetType;
+import be.isach.ultracosmetics.player.UltraPlayer;
+import com.cryptomorin.xseries.XMaterial;
+
+/**
+ * Represents an instance of a allay pet summoned by a player.
+ *
+ * @author Chris6ix
+ * @since 08-06-2022
+ */
+public class PetAllay extends Pet {
+    public PetAllay(UltraPlayer owner, UltraCosmetics ultraCosmetics) {
+        super(owner, ultraCosmetics, PetType.getByName("allay"), XMaterial.ALLAY_SPAWN_EGG);
+    }
+}
