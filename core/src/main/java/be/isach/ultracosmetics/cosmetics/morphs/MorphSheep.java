1ef7d84b17852901b7a8439a800a3aea84b67e3f
datatags <datatags@users.noreply.github.com>
2022-07-07 21:49:21 -0700

    Fix sheep error

--- core/src/main/java/be/isach/ultracosmetics/cosmetics/morphs/MorphSheep.java
+++ core/src/main/java/be/isach/ultracosmetics/cosmetics/morphs/MorphSheep.java
@@ -3,14 +3,16 @@ package be.isach.ultracosmetics.cosmetics.morphs;
 import be.isach.ultracosmetics.UltraCosmetics;
 import be.isach.ultracosmetics.cosmetics.type.MorphType;
 import be.isach.ultracosmetics.player.UltraPlayer;
-import me.libraryaddict.disguise.disguisetypes.watchers.SheepWatcher;
-import org.bukkit.Bukkit;
+
 import org.bukkit.DyeColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import com.cryptomorin.xseries.XSound;
+
+import me.libraryaddict.disguise.disguisetypes.watchers.SheepWatcher;
 
 /**
  * Represents an instance of a sheep morph summoned by a player.
@@ -20,7 +22,6 @@ import com.cryptomorin.xseries.XSound;
  */
 public class MorphSheep extends Morph {
     private long coolDown = 0;
-    private int count = 0;
 
     public MorphSheep(UltraPlayer owner, UltraCosmetics ultraCosmetics) {
         super(owner, MorphType.valueOf("sheep"), ultraCosmetics);
@@ -30,19 +31,23 @@ public class MorphSheep extends Morph {
     public void onLeftClick(PlayerInteractEvent event) {
         if ((event.getAction() == Action.LEFT_CLICK_AIR
                 || event.getAction() == Action.LEFT_CLICK_BLOCK) && event.getPlayer() == getPlayer()) {
-            if (coolDown > System.currentTimeMillis()) return;
             event.setCancelled(true);
+            if (coolDown > System.currentTimeMillis()) return;
             XSound.ENTITY_SHEEP_AMBIENT.play(event.getPlayer().getLocation(), 1.0f, 1.0f);
             SheepWatcher sheepWatcher = (SheepWatcher) getDisguise().getWatcher();
-            count = 0;
-            Bukkit.getScheduler().runTaskTimer(getUltraCosmetics(), () -> {
-                if (count > 9) {
-                    cancel();
-                    return;
+            new BukkitRunnable() {
+                private int count = 0;
+
+                @Override
+                public void run() {
+                    if (count > 9) {
+                        cancel();
+                        return;
+                    }
+                    sheepWatcher.setColor(DyeColor.values()[RANDOM.nextInt(DyeColor.values().length)]);
+                    count++;
                 }
-                sheepWatcher.setColor(DyeColor.values()[RANDOM.nextInt(DyeColor.values().length)]);
-                count++;
-            }, 0, 2);
+            }.runTaskTimer(getUltraCosmetics(), 0, 2);
             coolDown = System.currentTimeMillis() + 3000;
         }
     }
